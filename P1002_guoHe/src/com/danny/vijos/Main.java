85ae2c74586be021ea3533406cc75a98122d5c73
dannyZhou <zhoujian8585169@126.com>
2014-12-02 22:26:25 +0800

    2 and 5 is AC and other are w

--- P1002_guoHe/src/com/danny/vijos/Main.java
+++ P1002_guoHe/src/com/danny/vijos/Main.java
@@ -1,5 +1,7 @@
 package com.danny.vijos;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,16 +19,20 @@ public class Main {
 
 		int tmpNumber = 0;
 
-		//  这里 的 0 是用来添加最新的, 这里的 12 是 没有用, 为了防止 溢出
-		int process[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		// 这里 的 0 是用来添加最新的, 这里的 12 是 没有用, 为了防止 溢出
+		int process[] = { Integer.MAX_VALUE, Integer.MAX_VALUE,
+				Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE,
+				Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE,
+				Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE,
+				Integer.MAX_VALUE };
 
 		// 临时 变量存储地
 		String[] tmpStoneLocate;
-		
-		// 真实 存储 
-		int [] stoneLocate =  new int[100];
 
-		// 石头总数量
+		// 真实 存储
+		int[] stoneLocate = new int[102];
+
+		// 石头 数量 更改数量
 		int stoneNums = 0;
 
 		// flag
@@ -49,14 +55,30 @@ public class Main {
 		tmplineString = scanner.nextLine();
 
 		tmpStoneLocate = tmplineString.split(" ");
-		
-		for (int i =0 ; i<tmpStoneLocate.length; i++) {
-			stoneLocate[i] = Integer.parseInt(tmpStoneLocate[i]); 
+
+		for (int i = 0; i < tmpStoneLocate.length; i++) {
+			stoneLocate[i] = Integer.parseInt(tmpStoneLocate[i]);
 		}
 		Arrays.sort(stoneLocate, 0, tmpStoneLocate.length);
 
-		for (int i = 0; i <= n + 20; i++) {
-			
+		for (int i = 11 - endSpan; i <= 11 - beginSpan; i++) {
+
+			// 看看当前 有没有石头
+			if (stoneLocate[stoneNums] == i) {
+				process[i] = 1;
+				stoneNums++;
+				if (stoneNums == tmpNumber) {
+					flag = false;
+				}
+			} else {
+				process[i] = 0;
+			}
+
+		}
+		// 固定 i
+		int i = endSpan + 1;
+		while (i <= n + 10) {
+
 			if (flag) {
 
 				// 看看当前 有没有石头
@@ -73,6 +95,7 @@ public class Main {
 			} else {
 				process[0] = 0;
 			}
+
 			int tmpInt = Integer.MAX_VALUE;
 
 			// 取最小 区间内
@@ -82,21 +105,35 @@ public class Main {
 				}
 			}
 
-			process[0] += tmpInt;
+			if (tmpInt == Integer.MAX_VALUE) {
 
+				process[0] = Integer.MAX_VALUE;
+
+			} else {
+
+				process[0] += tmpInt;
+
+			}
 			// 所有元素后移一位
 			for (int j = 10; j >= 0; j--) {
 				process[j + 1] = process[j];
 			}
+			process[0] = 0;
+
+			// 计算 这个石子 和下一个石子的 距离
+			if (stoneNums != 0
+					&& stoneLocate[stoneNums] - stoneLocate[stoneNums - 1] > 2520) {
+				i = i + 2520;
+			}
+			i++;
 		}
- 
 		tmpNumber = Integer.MAX_VALUE;
-		for (int i = 0; i < process.length; i++) {
-			if (process[i]< tmpNumber && process[i]!= 0) {
-				tmpNumber  = process[i];
+		for (i = 1; i <= endSpan; i++) {
+			if (process[i] < tmpNumber && process[i] != 0) {
+				tmpNumber = process[i];
 			}
 		}
-		
+
 		System.out.println(tmpNumber);
 	}
-} 
+}
